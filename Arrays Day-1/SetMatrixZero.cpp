827d560db75f5ad3418529c2929afeaf1e65a00e
Bhavesh Upadhyay <92944256+brupadhyay@users.noreply.github.com>
2022-06-21 02:44:14 +0530

    Update SetMatrixZero.cpp

--- Arrays Day-1/SetMatrixZero.cpp
+++ Arrays Day-1/SetMatrixZero.cpp
@@ -1,5 +1,30 @@
-//Approach 1
+Time Complexity: O(2*(N*M)), as we are traversing two times in a matrix,
 
-//Approach 2
-
-//Approach 3
+Space Complexity: O(1).
+  
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+        for(int i =0; i<rows;i++){
+            if(matrix[i][0] == 0) col0 = 0;
+            for(int j = 1; j<cols;j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i= rows-1;i >= 0; i--){
+            for(int j = cols - 1; j >= 1 ; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+                    
+            }
+            if(col0 == 0){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
