122a6a44ae8bdeb311e1eb023b14495a2076fede
Bhavesh Upadhyay <92944256+brupadhyay@users.noreply.github.com>
2022-06-21 02:58:41 +0530

    Update RotateMatrix.cpp

--- Arrays Day-2/RotateMatrix.cpp
+++ Arrays Day-2/RotateMatrix.cpp
@@ -1,1 +1,19 @@
+Time Complexity: O(N*N) + O(N*N).One O(N*N) for transposing the matrix and the other for reversing the matrix.
 
+Space Complexity: O(1).
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i=0; i<n;i++){
+            for(int j=0; j<i;j++){
+                swap(matrix[i][j] , matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
