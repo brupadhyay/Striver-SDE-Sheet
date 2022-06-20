a2a7c63af2ebf2d563ff42180130a34076cc3da2
Bhavesh Upadhyay <92944256+brupadhyay@users.noreply.github.com>
2022-06-21 02:49:53 +0530

    Update PascalsTriangle.cpp

--- Arrays Day-1/PascalsTriangle.cpp
+++ Arrays Day-1/PascalsTriangle.cpp
@@ -1,4 +1,5 @@
-Time Complexity: We are creating a 2D array of size (numRows * numCols) (where 1 <= numCols <= numRows), and we are traversing through each of the cells to update it with its correct value, so Time Complexity = O(numRows2).
+Time Complexity: We are creating a 2D array of size (numRows * numCols) (where 1 <= numCols <= numRows),
+and we are traversing through each of the cells to update it with its correct value, so Time Complexity = O(numRows2).
 
 Space Complexity: Since we are creating a 2D array, space complexity = O(numRows2).
   
