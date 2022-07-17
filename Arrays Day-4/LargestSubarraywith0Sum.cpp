f8daf5e57b1ce5429a6733544174f680cd6041e7
Bhavesh Upadhyay <brupadhyay_b20@ee.vjti.ac.in>
2022-07-17 13:27:18 +0530

    Create LargestSubarraywith0Sum.cpp

---
+++ Arrays Day-4/LargestSubarraywith0Sum.cpp
@@ -0,0 +1,50 @@
+Solution 1: Naive Approach
+
+static int solve(int[] a){
+	int  max = 0;
+	for(int i = 0; i < a.length; ++i){
+		int sum = 0;
+		for(int j = i; j < a.length; ++j){
+			sum += a[j];
+			if(sum == 0){
+				max = Math.max(max, j-i+1);
+			}
+		}
+	}
+	return max;
+}
+
+TC - O(N^2)
+SC - O(1)
+  
+
+  
+Solution 2: Optimised Approach
+
+int maxLen(int A[], int n)
+{
+    // Your code here
+    unordered_map<int,int> mpp; 
+    int maxi = 0;
+    int sum = 0; 
+    for(int i = 0;i<n;i++) {
+        sum += A[i]; 
+        if(sum == 0) {
+            maxi = i + 1; 
+        }
+        else {
+            if(mpp.find(sum) != mpp.end()) {
+                maxi = max(maxi, i - mpp[sum]); 
+            }
+            else {
+                mpp[sum] = i;
+            }
+        }   
+    }
+
+    return maxi; 
+}
+
+Time Complexity: O(N), as we are traversing the array only once
+
+Space Complexity: O(N), in the worst case we would insert all array elements prefix sum into our hashmap
